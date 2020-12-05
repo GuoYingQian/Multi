436fe6670a13319e119a6e3eb699d42cc34dbbe1
Shaun Qian <sq74415@gmail.com>
2020-12-04 22:24:49 -0500

    Add keyboard and other codes

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,7 +9,8 @@
 			<button :class="['btn', calcumulti?'checked':'unchecked']" @tap="chooseType('multi')">a x b</button>
 			<button :class="['btn', calcumulti?'unchecked':'checked']" @tap="chooseType('posneg')">a - (-b)</button>
 		</view>
-		<view class="times">
+		<view class="infos">
+			<text class="currentitem" v-show="isdoing">{{currentitem}}</text>
 			<text class="showtime" @tap="stopTimer">{{showtime}}</text>
 		</view>
 		<view class="messages">
@@ -19,8 +20,34 @@
 		<view class="text-area">
 			<text class="expression">{{expression}}</text>
 		</view>
-		<view class="input">
-			<input v-model="answer" :placeholder="inputtooltip"/>
+		<view class="input" ref="answer" v-show="isdoing">
+			<input v-model="answer" :placeholder="inputtooltip" @focus="needInput(true)" />
+		</view>
+		<view class="resultmessage" v-show="showresultmsg">
+			<view>{{resultmessage}}</view>
+		</view>
+		<view class="keyboard" v-show="isdoing">
+			<view class="keyrow">
+				<button class="keyitem" @tap="keyIn('7')">7</button>
+				<button class="keyitem" @tap="keyIn('8')">8</button>
+				<button class="keyitem" @tap="keyIn('9')">9</button>
+			</view>
+			<view class="keyrow">
+				<button class="keyitem" @tap="keyIn('4')">4</button>
+				<button class="keyitem" @tap="keyIn('5')">5</button>
+				<button class="keyitem" @tap="keyIn('6')">6</button>
+			</view>
+			<view class="keyrow">
+				<button class="keyitem" @tap="keyIn('1')">1</button>
+				<button class="keyitem" @tap="keyIn('2')">2</button>
+				<button class="keyitem" @tap="keyIn('3')">3</button>
+			</view>
+			<view class="keyrow">
+				<button class="keyitem" @tap="keyIn('-')">-</button>
+				<button class="keyitem" @tap="keyIn('0')">0</button>
+				<button class="keyitem" @tap="keyIn('D')">D</button>
+			</view>
+			<button class="keyitem" @tap="keyIn('E')">E</button>
 		</view>
 	</view>
 </template>
@@ -35,29 +62,41 @@
 				result: 0,
 				answer: '',
 				calcumulti: false,
+				isdoing: false,
+				showresultmsg: false,
+				
+				timesecond: 0,
+				time: null,
+				
+				minmulti:0,
+				maxmulti:9,
+				minposneg:-20,
+				maxposneg:20,
+				
+				itemsize:5,
+				itemdone:0,
+				
 				curlang: 'ch',
 				res: {
 					"ch" : {
 					hintmessage: 'Tap to start Chinese',
 					inputtooltip: 'input the answer Chinese',
+					resultmessage: 'Good, you have finished the exersize',
 					},
 					"en" : {
 					hintmessage: 'Tap to start English',
 					inputtooltip: 'input the answer English',
+					resultmessage: 'Good, you have finished the exersize',
 					},
 					"fr" : {
 					hintmessage: 'Tap to start French',
 					inputtooltip: 'input the answer French',
+					resultmessage: 'Good, you have finished the exersize',
 					},
 				},
-				timesecond: 0,
-				time: null,
-				minmulti:0,
-				maxmulti:9,
-				minposneg:-20,
-				maxposneg:20,
 			};
 		},
+		
 		computed:{
 			hintmessage(){
 				return this.res[this.curlang].hintmessage
@@ -65,10 +104,17 @@
 			inputtooltip(){
 				return this.res[this.curlang].inputtooltip
 			},
+			resultmessage(){
+				return this.res[this.curlang].resultmessage
+			},
 			showtime(){
 				return ('0' + Math.floor(this.timesecond / 60)).slice(-2) + ' : ' + ('0' + this.timesecond % 60).slice(-2)
 			},
+			currentitem(){
+				return (this.itemdone+1) + ' / ' + this.itemsize 
+			}
 		},
+		
 		onLoad() {
 		},
 		
@@ -77,8 +123,20 @@
 				this.expression = ''
 				this.result = 0
 				this.answer = ''
-				this.inputtooltip = ''
+				this.itemdone = 0
+				this.keyvisible = false
 			},
+			
+			beginDo(){
+				this.showresultmsg = false
+				this.isdoing = true
+			},
+			
+			endDo(){
+				this.isdoing = false
+				this.showresultmsg = true
+			},
+			
 			getrandom(min, max){
 				return min + Math.floor(Math.random()*(max-min+1));
 			},
@@ -106,7 +164,6 @@
 			},
 			
 			getNewExpression(){
-				this.imagesrc = (this.imagesrc === '/static/logo1.png'?'/static/logo.png':'/static/logo1.png');
 				if(this.calcumulti === true){
 					this.getMulti();
 				}else{
@@ -116,10 +173,14 @@
 			},
 			
 			startQues(){
+				this.initcontent()
+				
 				this.stopTimer()
 				this.timesecond = 0
 				this.getNewExpression()
 				this.startTimer()
+				
+				this.beginDo()
 			},
 			
 			chooseType(type){
@@ -131,12 +192,50 @@
 				this.curlang = lang
 			},
 			
+			needInput(need){
+				this.keyvisible=need
+			},
+			
+			keyIn(key){
+				switch(key)
+				{
+					case 'D':
+						this.answer=this.answer.slice(0, this.answer.length-1)
+						break
+					case '-':
+						if(this.answer === '') this.answer = key
+						break
+					case 'E':
+						if(this.answer == this.result){
+							this.itemdone++
+							if(this.itemdone < this.itemsize){
+								this.getNewExpression()
+							}
+							else{
+								this.stopTimer()
+								this.initcontent()
+								this.endDo()
+							}	
+						}
+						else{
+							this.answer=''
+							this.imagesrc = '/static/logo1.png'
+							setTimeout(()=>{this.imagesrc = '/static/logo.png'},1000)
+						}
+						this.$refs.answer.$el.focus()
+						break
+					default:
+						this.answer += key
+				}
+			},
+			
 			startTimer(){
 				this.timer = setInterval(()=>{this.timesecond++}, 1000)
 			},
 			
 			stopTimer(){
-				console.log(this.timer)
+				clearInterval(this.timer)
+				this.timer=null
 			},
 		},
 	}
@@ -226,5 +325,17 @@
 		border: 3upx solid #808080;
 	}
 	
+	.keyboard{
+		display: flex;
+		flex-direction: column;
+	}
 	
+	.keyrow{
+		display: flex;
+		flex-direction: row;
+	}
+	
+	.currentitem{
+		margin-right: 100upx;
+	}
 </style>
