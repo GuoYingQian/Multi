11531dd0d2a0e4c1d1e6e5253aa009782a64ff37
ShaunQian <shaunqian@gmail.com>
2020-12-10 23:00:10 -0500

    Use uni app style

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,54 +1,84 @@
 <template>
-	<view class="content">
-		<view class="langs">
-			<view class="lang" :class="{activelang: curlang==='ch'}" @tap="changeLang('ch')">中文</view>
-			<view class="lang" :class="{activelang: curlang==='en'}" @tap="changeLang('en')">English</view>
-			<view class="lang" :class="{activelang: curlang==='fr'}" @tap="changeLang('fr')">Français</view>
+	
+	<view class="uni-padding-wrap uni-common-mt">
+		<view class="uni-flex uni-row">
+			<view class="lang" :class="{activelang: curlang==='ch'}" style="-webkit-flex: 1;flex: 1;" @tap="changeLang('ch')">中文</view>
+			<view class="lang" :class="{activelang: curlang==='en'}" style="-webkit-flex: 1;flex: 1;" @tap="changeLang('en')">English</view>
+			<view class="lang" :class="{activelang: curlang==='fr'}" style="-webkit-flex: 1;flex: 1;" @tap="changeLang('fr')">Français</view>
 		</view>
-		<view class="btns">
+		<view class="uni-flex uni-row" style="-webkit-justify-content: space-between;justify-content: space-between;">
 			<button :class="['btn', calcumulti?'checked':'unchecked']" @tap="chooseType('multi')">a x b</button>
 			<button :class="['btn', calcumulti?'unchecked':'checked']" @tap="chooseType('posneg')">a - (-b)</button>
 		</view>
-		<view class="infos">
-			<text class="currentitem" v-show="isdoing">{{currentitem}}</text>
-			<text class="showtime" @tap="stopTimer">{{showtime}}</text>
+		<view class="uni-flex uni-row" style="-webkit-justify-content: space-between;justify-content: space-between;">
+			<text class="showtime text" @tap="stopTimer">{{showtime}}</text>
+			<text class="currentitem text" v-show="isdoing">{{currentitem}}</text>
 		</view>
-		<view class="messages">
+		<view class="uni-flex uni-row" style="-webkit-justify-content: center;justify-content: center;">
 			<image class="logo" @tap="startQues" :src='imagesrc'></image>
 		</view>
-		<view class="text-area">
-			<text class="expression">{{expression}}</text>
+		<view class="uni-flex uni-row" style="-webkit-justify-content: center;justify-content: center;">
+			<text class="text">{{expression}}</text>
 		</view>
-		<view class="input" ref="answer" v-show="isdoing">
-			<input v-model="answer" :placeholder="inputtooltip" @focus="needInput(true)" />
+		<view class="input uni-flex uni-row" ref="answer" v-show="isdoing" style="-webkit-justify-content: center;justify-content: center;">
+			<input class="text" style="-webkit-justify-content: center;justify-content: center;" v-model="answer" :placeholder="inputtooltip" @focus="needInput(true)" />
 		</view>
-		<view class="resultmessage" v-show="showresultmsg">
+		<view class="uni-flex uni-row" v-show="showresultmsg">
 			<view>{{resultmessage}}</view>
 		</view>
-		<view class="keyboard" v-show="isdoing">
-			<view class="keyrow">
+		<view v-show="isdoing">
+			<view class="uni-flex uni-row">
 				<button class="keyitem" @tap="keyIn('7')">7</button>
 				<button class="keyitem" @tap="keyIn('8')">8</button>
 				<button class="keyitem" @tap="keyIn('9')">9</button>
+				<button id="keydelete" class="keyitem imagekey" @tap="keyIn('D')">←</button>
 			</view>
-			<view class="keyrow">
+			<view class="uni-flex uni-row">
 				<button class="keyitem" @tap="keyIn('4')">4</button>
 				<button class="keyitem" @tap="keyIn('5')">5</button>
 				<button class="keyitem" @tap="keyIn('6')">6</button>
+				<button class="keyitem" @tap="keyIn('0')">0</button>
 			</view>
-			<view class="keyrow">
+			<view class="uni-flex uni-row">
 				<button class="keyitem" @tap="keyIn('1')">1</button>
 				<button class="keyitem" @tap="keyIn('2')">2</button>
 				<button class="keyitem" @tap="keyIn('3')">3</button>
-			</view>
-			<view class="keyrow">
 				<button class="keyitem" @tap="keyIn('-')">-</button>
-				<button class="keyitem" @tap="keyIn('0')">0</button>
-				<button class="keyitem" @tap="keyIn('D')">D</button>
 			</view>
-			<button class="keyitem" @tap="keyIn('E')">E</button>
+			
+			<view class="uni-flex uni-row">
+				<button id="keyenter" class="keyitem imagekey" @tap="keyIn('E')">{{entertext}}</button>
+			</view>
+
+			<!-- <view class="keymain uni-flex uni-column">
+				<view class="uni-flex uni-row">
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('7')">7</button>
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('8')">8</button>
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('9')">9</button>
+				</view>
+				<view class="uni-flex uni-row">
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('4')">4</button>
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('5')">5</button>
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('6')">6</button>
+				</view>
+				<view class="uni-flex uni-row">
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('1')">1</button>
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('2')">2</button>
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('3')">3</button>
+				</view>
+				<view class="uni-flex uni-row">
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('-')">-</button>
+					<button class="keyitem" style="-webkit-flex: 1;flex: 1;" @tap="keyIn('0')">0</button>
+					
+				</view>
+			</view>
+			<view class="keyenter uni-column">
+				<button class="keyitem" @tap="keyIn('D')">D</button>
+				<button class="keyitem" @tap="keyIn('E')">E</button>
+			</view> -->
 		</view>
 	</view>
+	
 </template>
 
 <script>
@@ -81,16 +111,19 @@
 					hintmessage: 'Tap to start Chinese',
 					inputtooltip: 'input the answer Chinese',
 					resultmessage: 'Good, you have finished the exersize',
+					entertext: 'Chinese Enter',
 					},
 					"en" : {
 					hintmessage: 'Tap to start English',
 					inputtooltip: 'input the answer English',
 					resultmessage: 'Good, you have finished the exersize',
+					entertext: 'English Enter',
 					},
 					"fr" : {
 					hintmessage: 'Tap to start French',
 					inputtooltip: 'input the answer French',
 					resultmessage: 'Good, you have finished the exersize',
+					entertext: 'French Enter',
 					},
 				},
 			};
@@ -105,6 +138,9 @@
 			},
 			resultmessage(){
 				return this.res[this.curlang].resultmessage
+			},
+			entertext(){
+				return this.res[this.curlang].entertext
 			},
 			showtime(){
 				return ('0' + Math.floor(this.timesecond / 60)).slice(-2) + ' : ' + ('0' + this.timesecond % 60).slice(-2)
@@ -125,6 +161,9 @@
 				this.itemdone = 0
 				this.keyvisible = false
 				this.isdoing = false
+				
+				this.stopTimer()
+				this.timesecond = 0
 			},
 			
 			beginDo(){
@@ -208,6 +247,8 @@
 					case 'E':
 						if(this.answer == this.result){
 							this.itemdone++
+							this.imagesrc = '/static/correctanswer.png'
+							setTimeout(()=>{this.imagesrc = '/static/logo.png'},1000)
 							if(this.itemdone < this.itemsize){
 								this.getNewExpression()
 							}
@@ -219,7 +260,7 @@
 						}
 						else{
 							this.answer=''
-							this.imagesrc = '/static/logo1.png'
+							this.imagesrc = '/static/wronganswer.png'
 							setTimeout(()=>{this.imagesrc = '/static/logo.png'},1000)
 						}
 						this.$refs.answer.$el.focus()
@@ -242,79 +283,31 @@
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 50rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-		display: inline-flex;
-		float: left;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.uni-row{
+		margin-top: 20rpx;
 	}
 	
-	.input {
-		border-bottom-style: solid;
-		text-align: center;
-	}
-	.messages_{
-		display: flex;
-		flex-direction: row;
-	}
-	.btns{
-		margin-top: 20upx;
-	}
-	.btn {
-		position: relative;
-		border: 0upx;
-		display: inline-flex;
-		align-items: center;
-		justify-content: center;
-		box-sizing: border-box;
-		padding: 0 30upx;
-		
-		
-		font-size: 28upx;
-		height: 64upx;
-		width: 240upx;
-		line-height: 1;
-		text-align: center;
-		text-decoration: none;
-		overflow: visible;
-		margin-left: initial;
-		transform: translate(0upx, 0upx);
-		margin-right: 20upx;
-		border-radius: 5000upx;
-	}
 	
 	.lang{
-		position: relative;
-		display: inline-block;
+		padding-bottom: 5rpx; 
 		text-align: center;
-		width: 150upx;
-		margin: 20upx;
-		padding-bottom: 10upx;
+		font-size: large;
 	}
 	
 	.activelang{
 		border-bottom: solid;
+	}
+	
+	.text{
+		font-size:x-large;
+	}
+	
+	.btn{
+		height: 70rpx;
+		width: 240rpx;
+		line-height: 70rpx;
+		border-radius: 5000rpx;
+		font-size: large;
 	}
 	
 	.checked {
@@ -324,18 +317,32 @@
 	.unchecked {
 		border: 3upx solid #808080;
 	}
-	
-	.keyboard{
-		display: flex;
-		flex-direction: column;
+
+	.input {
+		border-bottom-style: solid;
+		text-align: center;
 	}
 	
-	.keyrow{
-		display: flex;
-		flex-direction: row;
+	.keyitem{
+		-webkit-flex: 1;
+		flex: 1;
 	}
 	
-	.currentitem{
-		margin-right: 100upx;
+	#keydelete{
+		background: url(../../static/deletekey.png);
 	}
+	
+	#keyenter{
+		background: url(../../static/deletekey.png);
+	}
+	.imagekey{
+		background-position:center;
+		background-size: cover;
+	}
+	#keyenter image{
+		height: 100%;
+	}
+	/* #keydelete{
+		background: url('./static/deletekey.png');
+	} */
 </style>
